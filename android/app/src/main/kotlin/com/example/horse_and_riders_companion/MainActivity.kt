eed88751d508a500baf7f24f647d64015020f4ca
mfrenchy77 <mfrenchy77@gmail.com>
2024-12-04 09:24:42 -0800

    chore: Remove unused configuration files and update project settings for improved performance

---
+++ android/app/src/main/kotlin/com/example/horse_and_riders_companion/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.horse_and_riders_companion
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- android/app/src/main/kotlin/com/frenchfriedtechnlogy/horseandriderscompanion/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.frenchfriedtechnlogy.horseandriderscompanion
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
